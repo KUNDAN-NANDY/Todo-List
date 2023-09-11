ee354228f9c7dba59e519af30f805602b397497c
Kundan Nandy <kundannandy27598@gmail.com>
2023-09-11 16:35:36 +0530

    Adding Todo List structure  ans style

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ const db = require('./config/mongoose');
 // Set up the view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
-app.use(express.urlencoded());
-app.use(express.static('assets'));
+app.use(express.urlencoded({ extended: true }));
+app.use('/assets', express.static(__dirname + '/assets'));
 
 app.listen(port, function (err) {
     if(err){
